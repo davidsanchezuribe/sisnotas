04a74fc1fb32ff1c1e6954374ed1c6df81118975
David Sanchez <dei@Davids-iMac.local>
2020-09-03 18:40:33 -0500

    rutas david añadidas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/materia/crea', 'MateriaController@create')->name("materia.create");
+
+Route::get('/materia/lista', 'MateriaController@list')->name("materia.list");
+
+Route::get('/materia/muestra/{id}', 'MateriaController@show')->name("materia.show");
+
+Route::post('/materia/guarda', 'MateriaController@save')->name("materia.save");
+
+Route::post('/materia/actualiza', 'MateriaController@update')->name("materia.update");
+
+Route::get('/evaluacion/gestiona', 'EvaluacionController@admin')->name("evaluacion.admin");
+
+Route::post('/evaluacion/insertualiza', 'EvaluacionController@upsert')->name("evaluacion.upsert");
+
+Route::get('/nota/gestiona', 'NotaController@admin')->name("nota.admin");
+
+Route::post('/nota/insertualiza', 'NotaController@upsert')->name("nota.upsert");
