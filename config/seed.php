ee95e2d013509ab9ec7bc2df1eb414512779586d
David Sanchez <dei@Davids-iMac.local>
2020-09-03 17:00:58 -0500

    Fakers inteligentes

---
+++ config/seed.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'profesores' => 50,
+    'grados' => 20,
+    'estudiantes' => 100,
+    'materias' => 60,
+];
---
+++ database/migrations/2020_09_03_203852_create_notas_view.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+
+class CreateNotasView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE VIEW notasView AS
+                        SELECT evaluaciones.id AS evaluacion_id, 
+                        materias.id AS materia_id,
+                        estudiantes.id AS estudiante_id
+                        FROM evaluaciones
+                        INNER JOIN materias 
+                        ON evaluaciones.materia_id = materias.id
+                        INNER JOIN estudiantes 
+                        ON materias.grado_id = estudiantes.grado_id
+                        WHERE evaluaciones.fecha < CURRENT_DATE;");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("DROP VIEW notasView");
+    }
+}
